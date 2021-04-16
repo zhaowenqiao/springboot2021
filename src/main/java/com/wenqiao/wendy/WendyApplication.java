4d7add955117538eee83e2802fbcc6e5a02271bb
zhaowenqiao <wenqiao127@126.com>
2021-04-16 18:51:11 +0800

    first commit

---
+++ src/main/java/com/wenqiao/wendy/WendyApplication.java
@@ -0,0 +1,13 @@
+package com.wenqiao.wendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WendyApplication.class, args);
+    }
+
+}
