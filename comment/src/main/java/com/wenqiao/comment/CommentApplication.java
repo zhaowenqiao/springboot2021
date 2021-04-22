4a2cf612993ffebff3511a6af27345aa64e31afd
zhaowenqiao <wenqiao127@126.com>
2021-04-22 16:34:50 +0800

    'ad'

---
+++ comment/src/main/java/com/wenqiao/comment/CommentApplication.java
@@ -0,0 +1,13 @@
+package com.wenqiao.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
+
+}
